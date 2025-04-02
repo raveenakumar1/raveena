8a4b0760a53c39f4407759a578f364624b9f57c3
raveenakumar1 <raveenakumar574@gmail.com>
2025-04-02 08:08:21 -0700

    updated side bar

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+
+
+//loading screen animation
 window.addEventListener("load", function () {
     // wait a bit for effect
     setTimeout(() => {
@@ -6,9 +9,19 @@ window.addEventListener("load", function () {
     }, 2000); // 2 seconds delay for animation effect
 });
 
-var static = document.getElementById('static'),
-    eating_gif = 'https://raveenakumar1.github.io/raveena/images/eating-kitty.gif';
 
-static.onclick = function() {
-    static.src = eating_gif;
-}
+//shop screen feeder
+var staticImg = document.getElementById('static');
+var feedButton = document.querySelector('.feed-me button');
+var eatingGif = 'https://raveenakumar1.github.io/raveena/images/eating-kitty.gif';
+
+feedButton.addEventListener("click", function () {
+    if (staticImg.src !== eatingGif) { // gif plays ONCE
+        staticImg.src = eatingGif;
+
+
+        setTimeout(() => {
+            staticImg.src = 'https://raveenakumar1.github.io/raveena/images/eating-kitty-static.png'; // Reset to static
+        }, 1700); // duration of gif
+    }
+});
