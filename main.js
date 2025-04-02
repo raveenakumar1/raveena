8e4c34615e2300eec186b4e637566c626936f2b1
raveenakumar1 <raveenakumar574@gmail.com>
2025-04-01 18:54:21 -0700

    added sidebar

--- main.js
+++ main.js
@@ -5,3 +5,10 @@ window.addEventListener("load", function () {
         document.getElementById("main-content").style.display = "block";
     }, 2000); // 2 seconds delay for animation effect
 });
+
+var static = document.getElementById('static'),
+    eating_gif = 'https://raveenakumar1.github.io/raveena/images/eating-kitty.gif';
+
+static.onclick = function() {
+    static.src = eating_gif;
+}
